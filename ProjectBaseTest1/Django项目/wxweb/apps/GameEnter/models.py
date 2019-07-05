18b10fd2695704b0c916e4fcd80e3176be7a4017
your namesj_zxgit config --global user.name sj_zxgit config --global user.name your <970832396@qq.com>
2019-07-05 18:03:33 +0800

    补充数据库model

--- ProjectBaseTest1/Django项目/wxweb/apps/GameEnter/models.py
+++ ProjectBaseTest1/Django项目/wxweb/apps/GameEnter/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class SingUp(models.Model):
+    ID = models.IntegerField(primary_key=True, auto_created=True, verbose_name='自增ID')
+    Guid = models.CharField(max_length=32, verbose_name='唯一ID')
+    UserName = models.CharField(max_length=20, blank=False, verbase_name='用户名')
